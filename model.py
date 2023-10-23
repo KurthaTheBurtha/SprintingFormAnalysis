7c01c2fd7f25727db74ad81166bedf7529559314
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-10-22 22:20:10 -0700

    GridSearchCV

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 import numpy as np
-import matplotlib as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
 def train():
     # process inputs
@@ -11,24 +9,25 @@ def train():
     bad = np.load('bad.npy')
 
     # target values are 0 for bad form, 1 for good form
-    goodtargets = [1]*len(good)
-    badtargets = [0]*len(bad)
+    goodtargets = [1] * len(good)
+    badtargets = [0] * len(bad)
 
     # combines good and bad into one sest
-    inputs = np.concatenate((good,bad),axis=0)
-    target = goodtargets+badtargets
+    inputs = np.concatenate((good, bad), axis=0)
+    target = goodtargets + badtargets
 
     # split dataset into test and train
-    inputs_train, inputs_test, target_train, target_test = train_test_split(inputs,target,test_size=0.5, random_state=42)\
+    inputs_train, inputs_test, target_train, target_test = train_test_split(inputs, target, test_size=0.5, random_state=42)
 
-    # fit data on 50% of data
-    classifier = MLPClassifier(random_state=0, hidden_layer_sizes=(25,50,10,5),batch_size=5)
-    classifier.fit(inputs_train, target_train)
-
-    # get results and print
-    results = classifier.predict(inputs_test)
-    print(str(round((results == target_test).mean()*100,2))+'% accuracy')
-
+    model = MLPClassifier()
+    grid = GridSearchCV(model, {
+        'random_state': [0],
+        'hidden_layer_sizes': [(50,) * num_layers for num_layers in range(1, 5)],
+        'batch_size': [5, 10, 20, 50],
+        'learning_rate_init': [0.001, ]
+    }, verbose=1)
+    grid.fit(inputs_train, target_train)
+    breakpoint()
 
 if __name__ == '__main__':
     train()
